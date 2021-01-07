6016cb5a17e4dfbefc417ea3f445db5d192fa2da
Tyler Mumford <tyler.mumford@icloud.com>
2021-01-07 00:01:11 -0700

    Refactor click tests.

--- entities/game-clicks.spec.js
+++ entities/game-clicks.spec.js
@@ -3,22 +3,22 @@ import * as playerFuncs from './player.js';
 import { List } from "immutable";
 
 describe('game clicking', () => {
-    let g;
     let p1;
     let p2;
+    let initialGame;
     let resultGame;
     const validCoordinates = List.of(2, 5);
 
     // Helper functions
-    const clickFor = player => gameFuncs.performClick(g, player, validCoordinates);
-    const getSquareOf = player => resultGame.getIn(['fields', player.playerID, 'squares', 2, 5]);
+    const clickFor = (player, game) => gameFuncs.performClick(game ?? initialGame, player, validCoordinates);
+    const getSquareOf = (player, game) => (game ?? resultGame).getIn(['fields', player.playerID, 'squares', 2, 5]);
 
     beforeEach(() => {
-        g = gameFuncs.createGame();
+        initialGame = gameFuncs.createGame();
         p1 = playerFuncs.createNewPlayer('P1');
         p2 = playerFuncs.createNewPlayer('P2');
-        g = gameFuncs.addPlayerAndCreateField(g, p1);
-        g = gameFuncs.addPlayerAndCreateField(g, p2);
+        initialGame = gameFuncs.addPlayerAndCreateField(initialGame, p1);
+        initialGame = gameFuncs.addPlayerAndCreateField(initialGame, p2);
     })
 
     test('should have a function', () => {
@@ -28,28 +28,28 @@ describe('game clicking', () => {
     test('should throw an error if the player is not in the game', () => {
         const unknown = playerFuncs.createNewPlayer();
 
-        expect(() => gameFuncs.performClick(g, unknown, validCoordinates)).toThrow();
+        expect(() => gameFuncs.performClick(initialGame, unknown, validCoordinates)).toThrow();
     })
 
     test('should throw an error if the coordinates are invalid', () => {
         const badCoordinates = List.of(1000, 256);
 
-        expect(() => gameFuncs.performClick(g, p1, badCoordinates)).toThrow();
+        expect(() => gameFuncs.performClick(initialGame, p1, badCoordinates)).toThrow();
     })
 
     test('should not throw an error for coordinates in range', () => {
-        expect(() => gameFuncs.performClick(g, p1, validCoordinates)).not.toThrow();
+        expect(() => gameFuncs.performClick(initialGame, p1, validCoordinates)).not.toThrow();
     })
 
     test('should return a game', () => {
         resultGame = clickFor(p1)
 
         expect(resultGame).toBeDefined();
-        expect(resultGame.gameID).toBe(g.gameID);
+        expect(resultGame.gameID).toBe(initialGame.gameID);
     })
 
     test('should throw an error if the game is null', () => {
-        g = null;
+        initialGame = null;
 
         expect(() => clickFor(p1)).toThrow();
     })
@@ -70,7 +70,7 @@ describe('game clicking', () => {
 
     test('should reveal a square if all players have clicked it', () => {
         resultGame = clickFor(p1);
-        resultGame = gameFuncs.performClick(resultGame, p2, validCoordinates);
+        resultGame = clickFor(p2, resultGame);
 
         expect(getSquareOf(p1).isRevealed).toBe(true);
         expect(getSquareOf(p2).isRevealed).toBe(true);

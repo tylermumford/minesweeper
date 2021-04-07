bb98f34d5b00927ece6a5801e5f818fadf40d07a
Tyler Mumford <tyler.mumford@icloud.com>
2021-04-06 22:57:00 -0600

    Delete all code.

--- entities/index.js
+++
@@ -1,5 +0,0 @@
-export { default as player, createNewPlayer } from './player.js';
-export { default as square } from './square.js';
-export { default as field, createNewField } from './field.js';
-export { default as game } from './game.js';
-export * from './game.js';

cc9ce5b328143b62d3d921a63ee38a14d0032a40
Tyler Mumford <tyler.mumford@icloud.com>
2021-04-24 21:10:23 -0600

    Reload the page naively every 3s

--- static-assets/controllers.js
+++ static-assets/controllers.js
@@ -1,8 +1,12 @@
+import * as Turbo from '/static/turbo.es2017-esm.js?beta5'
+
 const application = Stimulus.Application.start();
 const Controller = Stimulus.Controller;
 
+
 function registerControllers() {
     application.register("button", ButtonController);
+    application.register("refresher", RefresherController);
 }
 
 class ButtonController extends Controller {
@@ -11,6 +15,30 @@ class ButtonController extends Controller {
     setFlag(event) {
         event.preventDefault();
         this.flagSubmitTarget?.click();
+    }
+}
+
+class RefresherController extends Controller {
+    polling
+
+    connect() {
+        this.element.addEventListener('click', () => this.reload())
+
+        this.startPolling()
+    }
+
+    disconnect() {
+        clearInterval(this.polling)
+    }
+
+    reload() {
+        Turbo.visit(document.location.href, { action: 'replace' })
+    }
+
+    startPolling() {
+        this.polling = setInterval(() => {
+            this.reload()
+        }, 3000);
     }
 }
 

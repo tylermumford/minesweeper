321578ae73908dba9459d041df8af559d9ab798f
Tyler Mumford <tyler.mumford@icloud.com>
2021-04-29 23:55:16 -0600

    Remove polling auto-updates
    They were proving too janky. I'm going to try using Turbo Streams
    instead.

--- static-assets/controllers.js
+++ static-assets/controllers.js
@@ -6,7 +6,6 @@ const Controller = Stimulus.Controller;
 
 function registerControllers() {
     application.register("button", ButtonController);
-    application.register("refresher", RefresherController);
 }
 
 class ButtonController extends Controller {
@@ -15,62 +14,6 @@ class ButtonController extends Controller {
     setFlag(event) {
         event.preventDefault();
         this.flagSubmitTarget?.click();
-    }
-}
-
-class RefresherController extends Controller {
-    polling
-    timeStartedPolling
-    lastSeenState
-
-    async connect() {
-        this.element.addEventListener('click', () => this.reload())
-
-        await this.getFirstState();
-        this.startPolling();
-    }
-
-    async getFirstState() {
-        const response = await fetch(document.location.href, { method: 'HEAD' });
-
-        const stateHeader = response.headers.get('X-Minesweeper-Game-State');
-        if (response.ok && stateHeader) {
-            this.lastSeenState = stateHeader;
-        }
-    }
-
-    startPolling() {
-        this.timeStartedPolling = new Date();
-
-        this.polling = setInterval(async () => {
-            await this.maybeReload()
-
-            const maxPollingTime = 5 * 60 * 1000;
-            if (new Date() - this.timeStartedPolling > maxPollingTime) {
-                clearInterval(this.polling);
-            }
-        }, 3000);
-    }
-
-    async maybeReload() {
-        const response = await fetch(document.location.href, {method: 'HEAD'});
-
-        const stateHeader = response.headers.get('X-Minesweeper-Game-State');
-        if (!response.ok || !stateHeader) {
-            return;
-        }
-
-        if (stateHeader !== this.lastSeenState) {
-            this.reload();
-        }
-    }
-
-    reload() {
-        Turbo.visit(document.location.href, { action: 'replace' });
-    }
-
-    disconnect() {
-        clearInterval(this.polling)
     }
 }
 

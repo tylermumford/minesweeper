8bdf215915b41b3c0b19b4dda775a1c3caaa578b
Tyler Mumford <tyler.mumford@icloud.com>
2023-01-24 20:54:42 -0700

    Add touch gestures

--- static-assets/controllers.js
+++ static-assets/controllers.js
@@ -1,14 +1,20 @@
 import * as Turbo from '/static/turbo.es2017-esm.js?beta5'
+import '/static/hammer.min.js';
 
 const application = Stimulus.Application.start();
 const Controller = Stimulus.Controller;
 
 
+// Called once (at the bottom of this file) to connect
+// controllers to Stimulus.
 function registerControllers() {
     application.register("button", ButtonController);
     application.register("game-stream", GameStreamController);
+    application.register("gestures", GesturesController);
 }
 
+// Wires up an event handler for flagging a square
+// on the minefield.
 class ButtonController extends Controller {
     static get targets() { return ['flagSubmit']; }
 
@@ -18,6 +24,8 @@ class ButtonController extends Controller {
     }
 }
 
+// Wires up the Server-Sent Events connection with Turbo.
+// Implements a timeout to end the connection.
 class GameStreamController extends Controller {
     static get targets() { return ["timeoutMessage"]; }
 
@@ -30,12 +38,22 @@ class GameStreamController extends Controller {
             this.timeoutMessageTarget.classList.remove("dn");
             this.disconnect();
         }, time20Minutes);
+
+        document.addEventListener('turbo:before-visit', this.disconnect)
     }
 
     disconnect() {
         Turbo.disconnectStreamSource(this.connection);
         this.connection.close();
         clearTimeout(this.staleGameTimeout);
+    }
+}
+
+// Lets the Hammer library know about an element.
+class GesturesController extends Controller {
+    connect() {
+        this.hammerRef = new Hammer(this.element);
+        // The long-press event is wired up in the DOM.
     }
 }
 
